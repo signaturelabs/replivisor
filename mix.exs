c1e8104c65d59c912120e9b6829dd6da38003268
Traun Leyden <tleyden@branchcut.com>
2013-04-09 12:29:54 -0700

    couchbeam and deps

--- mix.exs
+++ mix.exs
@@ -13,8 +13,11 @@ defmodule Replivisor.Mixfile do
   end
 
   # Returns the list of dependencies in the format:
-  # { :foobar, "0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
-    []
+    [{:mochiweb, ".*", git: "http://github.com/mochi/mochiweb.git"},
+     {:jiffy, ".*", git: "http://github.com/refuge/jiffy.git"},
+     {:oauth, ".*", git: "http://github.com/refuge/erlang-oauth.git"},
+     {:ibrowse, ".*", git: "http://github.com/cmullaparthi/ibrowse.git"},
+     {:couchbeam, ".*", git: "https://github.com/benoitc/couchbeam.git"}]
   end
 end
