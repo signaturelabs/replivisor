6811e9f4d7a6724dc2793707e2b6da9dcdd996b8
Traun Leyden <tleyden@branchcut.com>
2013-04-09 13:03:48 -0700

    verify can connect to couch via couchbeam

--- test/replivisor_test.exs
+++ test/replivisor_test.exs
@@ -1,9 +1,19 @@
 Code.require_file "../test_helper.exs", __FILE__
 
 defmodule ReplivisorTest do
+
   use ExUnit.Case
 
   test "the truth" do
+
+    options = []
+    server = :couchbeam.server_connection "localhost", 5984, "", options 
+    result = :couchbeam.server_info server
+
+    IO.puts "server: #{inspect(server)}"
+    IO.puts "result: #{inspect(result)}" 
+
     assert(true)
+
   end
 end
