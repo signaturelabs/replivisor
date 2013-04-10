19618c343dbcc452a87917a2e4a174cb5098c67f
Traun Leyden <tleyden@branchcut.com>
2013-04-10 14:32:11 -0700

    move config to json file

--- lib/replivisor/config.ex
+++ lib/replivisor/config.ex
@@ -12,18 +12,32 @@ defmodule Replivisor.Config do
 		           couchbeam_change_pid: nil
 
 	def databases do
-		[ CouchDB.new(source_url: "localhost", 
-                              source_port: 5984, 
-                              source_dbname: "sigdb",
-		              target_url: "localhost", 
-                              target_port: 5984, 
-                              target_dbname: "sigdb-copy"),  
-		  CouchDB.new(source_url: "localhost", 
-                              source_port: 5984, 
-                              source_dbname: "sigdb-prod",
-		              target_url: "localhost", 
-                              target_port: 5984, 
-                              target_dbname: "sigdb-prod-copy") ]
+
+		{:ok, filedata} = File.read("config.json")
+		json = :jiffy.decode(filedata)
+		
+		couchdbs = []
+		f = fn json_elt, couchdbs ->
+		        couchdb = CouchDB.new()
+		        {inner_elt} = json_elt  # unwrap from tuple - why is this even needed?
+		        source_url = :proplists.get_value "source_url", inner_elt
+		        couchdb = couchdb.source_url(source_url)
+		        source_port = :proplists.get_value "source_port", inner_elt
+		        couchdb = couchdb.source_port(source_port)
+		        source_dbname = :proplists.get_value "source_dbname", inner_elt
+		        couchdb = couchdb.source_dbname(source_dbname)
+		        target_url = :proplists.get_value "target_url", inner_elt
+		        couchdb = couchdb.target_url(target_url)
+		        target_port = :proplists.get_value "target_port", inner_elt
+		        couchdb = couchdb.target_port(target_port)
+		        target_dbname = :proplists.get_value "target_dbname", inner_elt
+		        couchdb = couchdb.target_dbname(target_dbname)
+		        couchdbs = [couchdb | couchdbs]
+		        {json_elt, couchdbs}
+	        end
+		{_, couchdbs} = Enum.map_reduce json, couchdbs, f
+		couchdbs
+
 	end
 
 end
