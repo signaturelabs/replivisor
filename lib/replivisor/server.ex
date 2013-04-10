02c95eac012515634660333b447bf9789cd4a055
Traun Leyden <tleyden@branchcut.com>
2013-04-09 18:36:35 -0700

    track the change ref in a hashdict

--- lib/replivisor/server.ex
+++ lib/replivisor/server.ex
@@ -2,28 +2,29 @@ defmodule Replivisor.Server do
 	use GenServer.Behaviour
 
 	def start_link do
-		:gen_server.start_link({:local, __MODULE__}, __MODULE__, [], [])	
+		:gen_server.start_link({:local, __MODULE__}, __MODULE__, HashDict.new, [])	
 	end
 
-
 	def init(state) do
-		IO.puts "init called"
-		db = init_db
-		init_track_changes db
+		IO.puts "init called with state: #{inspect(state)}"
+		db = init_db()
+		start_ref = init_track_changes(db)
+		state = HashDict.put(state, start_ref, :empty)
+		IO.puts "init returning state: #{inspect(state)}"
 		{ :ok, state }
 	end
 
 	def handle_info(msg, state) do
-		IO.puts "handle_info called"
+		IO.puts "handle_info called with: #{inspect(msg)}"
 		{ :noreply, state }
 	end
 
 	def init_db do
 		options = []
-		server = :couchbeam.server_connection "localhost", 5984, "", options 
-		result = :couchbeam.server_info server
+		server = :couchbeam.server_connection("localhost", 5984, "", options)
+		result = :couchbeam.server_info(server)
 		db_options = []
-		{ok, db} = :couchbeam.open_db server, "sigdb", db_options
+		{ok, db} = :couchbeam.open_db(server, "sigdb", db_options)
 		db
 	end
 
@@ -34,7 +35,9 @@ defmodule Replivisor.Server do
 		changes_options = [:continuous, :heartbeat, since_tuple]
 		server_pid = :erlang.whereis(__MODULE__)
 		IO.puts "server_pid: #{inspect(server_pid)}"
-		{:ok, _start_ref, _change_pid} = :couchbeam_changes.stream(db, server_pid, changes_options)
+		{:ok, start_ref, change_pid} = :couchbeam_changes.stream(db, server_pid, changes_options)
+		IO.puts "start_ref: #{inspect(start_ref)} change_pid: #{inspect(change_pid)}"
+		start_ref
 	end
 
 end
